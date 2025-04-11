43c42130bf22caa7c4ce96db686a47a29041a5d4
Hossein Yasinzadeh <hosseinyasinzadeh2002@gmail.com>
2025-04-11 13:31:36 +0330

    feat: add appBar to Other File

---
+++ src/Components/AppBars.jsx
@@ -0,0 +1,21 @@
+import AppBar from "@mui/material/AppBar";
+import { Toolbar } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
+import { Box } from "@mui/material";
+
+const AppBars = () => {
+  return (
+    <AppBar color="error">
+      <Toolbar>
+        <Typography variant="h4">Todo List</Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <Button variant="contained" color="success">
+          Register
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppBars;
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,21 +1,9 @@
-import AppBar from "@mui/material/AppBar";
-import { Toolbar } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
-import { Box } from "@mui/material";
+import AppBars from "./AppBars";
 
 const Login = () => {
   return (
     <header>
-      <AppBar color="error">
-        <Toolbar>
-          <Typography variant="h4">Todo List</Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <Button variant="contained" color="success">
-            Register
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <AppBars />
     </header>
   );
 };
